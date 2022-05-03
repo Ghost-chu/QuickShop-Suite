45666357fdaca381790604686b932a445786aaf8
Ghost_chu <2908803755@qq.com>
2022-05-03 21:13:25 +0800

    Fix quickshop-suite-list plugin for 1.1.3.6

--- quickshop-suite-list/src/main/java/com/ghostchu/quickshopsuite/list/ListCommand.java
+++ quickshop-suite-list/src/main/java/com/ghostchu/quickshopsuite/list/ListCommand.java
@@ -4,19 +4,18 @@ package com.ghostchu.quickshopsuite.list;
 import com.ghostchu.quickshop.QuickShop;
 import com.ghostchu.quickshop.api.command.CommandHandler;
 import com.ghostchu.quickshop.api.shop.Shop;
-import com.ghostchu.quickshop.shade.net.kyori.adventure.text.Component;
-import com.ghostchu.quickshop.shade.net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import com.ghostchu.quickshop.util.Util;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ListCommand implements CommandHandler<Player> {
     private QuickShopSuiteList plugin;
@@ -45,25 +44,16 @@ public class ListCommand implements CommandHandler<Player> {
         }
         for (int i = 0; i < shops.size(); i++) {
             Shop shop = shops.get(i);
-            String message = plugin.getConfig().getString("lang.coord").replace("{num}", String.valueOf(i + 1)).replace("{name}", LegacyComponentSerializer.legacySection().serialize(Util.getItemStackName(shop.getItem())));
-            Component component = LegacyComponentSerializer.legacySection().deserialize(message);
-            List<String> lores = new ArrayList<>();
-            String title = null;
+            Component component = LegacyComponentSerializer.legacySection().deserialize(plugin.getConfig().getString("lang.coord"));
+            component = component.replaceText(TextReplacementConfig.builder().matchLiteral("{num}").replacement( String.valueOf(i + 1)).build());
+            component = component.replaceText(TextReplacementConfig.builder().matchLiteral("{name}").replacement(Util.getItemStackName(shop.getItem())).build());
             List<String> hover = plugin.getConfig().getStringList("lang.hover");
-
-            for (int j = 0; j < hover.size(); j++) {
-                if (j == 0) {
-                    title = format(shop, hover.get(j));
-                    continue;
-                }
-                lores.add(format(shop, hover.get(j)));
+            List<Component> description = hover.stream().map(str->LegacyComponentSerializer.legacySection().deserialize(str)).collect(Collectors.toList());
+            Component showComponent = Component.empty();
+            for (Component component1 : description) {
+                showComponent = showComponent.append(component1).append(Component.newline());
             }
-            ItemStack stack = new ItemStack(Material.STONE);
-            ItemMeta meta = stack.getItemMeta();
-            meta.setDisplayName(title);
-            meta.setLore(lores);
-            stack.setItemMeta(meta);
-            plugin.getAudience().player(commandSender).sendMessage(component.hoverEvent(QuickShop.getInstance().getPlatform().getItemStackHoverEvent(stack).asHoverEvent()));
+            plugin.getAudience().player(commandSender).sendMessage(component.hoverEvent(HoverEvent.showText(showComponent.compact())));
         }
     }
 
