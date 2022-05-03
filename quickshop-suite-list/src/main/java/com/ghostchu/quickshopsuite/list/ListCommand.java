0ee3ac9f9ed88f6b4af0eda180c3c331e7ae30e7
Ghost_chu <2908803755@qq.com>
2022-05-03 21:22:20 +0800

    Fix color parse

--- quickshop-suite-list/src/main/java/com/ghostchu/quickshopsuite/list/ListCommand.java
+++ quickshop-suite-list/src/main/java/com/ghostchu/quickshopsuite/list/ListCommand.java
@@ -43,10 +43,10 @@ public class ListCommand implements CommandHandler<Player> {
         for (int i = 0; i < shops.size(); i++) {
             Shop shop = shops.get(i);
             Component component = LegacyComponentSerializer.legacySection().deserialize(plugin.getConfig().getString("lang.coord"));
-            component = Helper.replaceArgs(component,"{num}", String.valueOf(i + 1));
-            component = Helper.replaceArgs(component,"{name}",Util.getItemStackName(shop.getItem()));
+            component = Helper.replaceArgs(component, "{num}", String.valueOf(i + 1));
+            component = Helper.replaceArgs(component, "{name}", Util.getItemStackName(shop.getItem()));
             List<String> hover = plugin.getConfig().getStringList("lang.hover");
-            List<Component> description = hover.stream().map(str->LegacyComponentSerializer.legacySection().deserialize(str)).collect(Collectors.toList());
+            List<Component> description = hover.stream().map(str -> LegacyComponentSerializer.legacyAmpersand().deserialize(str)).collect(Collectors.toList());
             Component showComponent = Component.empty();
             for (Component component1 : description) {
                 component1 = Helper.replaceArgs(component1, "{name}", Util.getItemStackName(shop.getItem()));
@@ -54,7 +54,7 @@ public class ListCommand implements CommandHandler<Player> {
                 component1 = Helper.replaceArgs(component1, "{x}", String.valueOf(shop.getLocation().getBlockX()));
                 component1 = Helper.replaceArgs(component1, "{y}", String.valueOf(shop.getLocation().getBlockY()));
                 component1 = Helper.replaceArgs(component1, "{z}", String.valueOf(shop.getLocation().getBlockZ()));
-                component1 = Helper.replaceArgs(component1, "{price}",QuickShop.getInstance().getEconomy().format(shop.getPrice(), shop.getLocation().getWorld(), shop.getCurrency()));
+                component1 = Helper.replaceArgs(component1, "{price}", QuickShop.getInstance().getEconomy().format(shop.getPrice(), shop.getLocation().getWorld(), shop.getCurrency()));
                 component1 = Helper.replaceArgs(component1, "{type}", shop.isSelling() ? plugin.getConfig().getString("lang.selling") : plugin.getConfig().getString("lang.buying"));
                 showComponent = showComponent.append(component1).append(Component.newline());
             }
